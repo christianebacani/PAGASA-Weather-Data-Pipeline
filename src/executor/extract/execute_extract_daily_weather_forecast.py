eb07002dc84beba29881e0b0f3a9f10ae354c6ba
christianebacani <christianbacani581@gmail.com>
2025-12-07 09:57:06 +0800

    fix: Docstring content of the module

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -1,5 +1,5 @@
 '''
     Module for executing extraction functions for the
     daily weather forecast data from the data/stage
-    subdirectory on the local machins.
+    subdirectory on the local machine.
 '''
