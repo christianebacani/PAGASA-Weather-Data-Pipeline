f05a6e2e826ed9b3808221559c3c6e6bdea0e410
christianebacani <christianbacani581@gmail.com>
2025-12-07 11:50:47 +0800

    feat: Funct 'parse_synopsis_to_dataframe'

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -6,6 +6,7 @@
 from etl.extract.extract_daily_weather_forecast import create_subdir
 from etl.extract.extract_daily_weather_forecast import parse_issued_datetime_to_dataframe
 from etl.extract.extract_daily_weather_forecast import stage_issued_datetime_dataframe
+from etl.extract.extract_daily_weather_forecast import parse_synopsis_to_dataframe
 
 def extract_daily_weather_forecast(
 ) -> None:
@@ -24,4 +25,8 @@ def extract_daily_weather_forecast(
     )
     stage_issued_datetime_dataframe(
         issued_datetime_dataframe
+    )
+
+    synopsis_dataframe = parse_synopsis_to_dataframe(
+        'data/raw/daily_weather_forecast/synopsis.json'
     )
