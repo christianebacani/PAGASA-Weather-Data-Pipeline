5f31382c75cf04e27bfd47b087d0effb95e16b04
christianebacani <christianbacani581@gmail.com>
2025-12-07 10:02:58 +0800

    feat: Funct 'extract_daily_weather_forecast'

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -3,3 +3,12 @@
     daily weather forecast data from the data/stage
     subdirectory on the local machine.
 '''
+def extract_daily_weather_forecast(
+) -> None:
+    '''
+    Extract the daily weather forecast from the
+    data/stage subdirectory on the local machine
+    by executing all functions in the
+    extract_daily_weather_forecast module
+    of the src/ingest package.    
+    '''
