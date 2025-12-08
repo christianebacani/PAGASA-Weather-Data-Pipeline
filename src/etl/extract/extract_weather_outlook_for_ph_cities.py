4c7085db641e8707090decefae67c7dc70ec1183
christianebacani <christianbacani581@gmail.com>
2025-12-08 10:08:33 +0800

    feat: Content inside the funct 'create_subdir'

--- src/etl/extract/extract_weather_outlook_for_ph_cities.py
+++ src/etl/extract/extract_weather_outlook_for_ph_cities.py
@@ -16,3 +16,6 @@ def create_subdir(
         cities from the data/raw subdirectory
         on the local machine.
     '''
+    # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
+    if not os.path.exists('data/stage/weather_outlook_for_ph_cities'):
+        os.makedirs('data/stage/weather_outlook_for_ph_cities')
