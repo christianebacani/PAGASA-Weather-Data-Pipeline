aa9eb3b0ee2df99f3a80ed4c3bda111bab74aa72
christianebacani <christianbacani581@gmail.com>
2025-12-08 10:14:26 +0800

    feat: Funct 'ingest_weather_outlook_for_ph_cities'

--- src/executor/extract/execute_extract_weather_outlook_for_ph_cities.py
+++ src/executor/extract/execute_extract_weather_outlook_for_ph_cities.py
@@ -3,3 +3,14 @@
     selected Philippine cities data from the data/stage
     subdirectory on the local machine.
 '''
+
+def ingest_weather_outlook_for_ph_cities(
+) -> None:
+    '''
+        Extracts the weather outlook for selected
+        Philippine cities from the data/raw
+        subdirectory on the local machine by
+        executing all functions in the
+        extract_weather_outlook_for_ph_cities
+        module of the src/ingest package.
+    '''
