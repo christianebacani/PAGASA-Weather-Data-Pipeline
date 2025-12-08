6f0ab7574b989a934c49f782eedb85712b131975
christianebacani <christianbacani581@gmail.com>
2025-12-08 11:24:55 +0800

    feat: Funct 'parse_issued_datetime_to_dataframe'

--- src/etl/extract/extract_weather_outlook_for_ph_cities.py
+++ src/etl/extract/extract_weather_outlook_for_ph_cities.py
@@ -19,3 +19,23 @@ def create_subdir(
     # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/stage/weather_outlook_for_ph_cities'):
         os.makedirs('data/stage/weather_outlook_for_ph_cities')
+
+def parse_issued_datetime_to_dataframe(
+        issued_datetime_filepath: str
+) -> pd.DataFrame:
+    '''
+    Parses the issued datetime from JSON
+    file into a DataFrame object.
+
+    :param issued_datetime_filepath: Relative
+        filepath of the JSON file that contains
+        the issued datetime of the weather
+        outlook for selected Philippine
+        cities
+    :type issued_datetime_filepath: str
+
+    :return: Issued datetime of the weather
+        outlook for selected Philippine cities
+        as a DataFrame object
+    :rtype: DataFrame
+    '''
