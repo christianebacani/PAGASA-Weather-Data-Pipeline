0b1ffb3ffe420f0cd28f9499e0b22d9b4e924307
christianebacani <christianbacani581@gmail.com>
2025-12-08 11:28:16 +0800

    feat: Funct 'create_subdir'

--- src/executor/extract/execute_extract_weather_outlook_for_ph_cities.py
+++ src/executor/extract/execute_extract_weather_outlook_for_ph_cities.py
@@ -3,6 +3,7 @@
     selected Philippine cities data from the data/raw
     subdirectory on the local machine.
 '''
+from etl.extract.extract_weather_outlook_for_ph_cities import create_subdir
 
 def ingest_weather_outlook_for_ph_cities(
 ) -> None:
@@ -14,3 +15,5 @@ def ingest_weather_outlook_for_ph_cities(
         extract_weather_outlook_for_ph_cities
         module of the src/ingest package.
     '''
+    # Run all functions to ingest weather advisory data
+    create_subdir()
