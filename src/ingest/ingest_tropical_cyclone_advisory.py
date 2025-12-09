6805edfc346de940e096f3a8ff3ffe0492488ac6
christianebacani <christianbacani581@gmail.com>
2025-12-09 13:38:48 +0800

    refact: Docstring content of the module

--- src/ingest/ingest_tropical_cyclone_advisory.py
+++ src/ingest/ingest_tropical_cyclone_advisory.py
@@ -1,5 +1,11 @@
-'''
-    Module to ingest tropical cyclone
-    advisory from the PAGASA-DOST
-    website.
-'''
+"""
+Ingest tropical cyclone advisory data from the PAGASA-DOST website.
+
+This module provides functions to extract key information from the
+tropical cyclone advisory page, including:
+
+- TBD
+
+All extracted data is saved as a JSON file in the
+`data/raw/weather_advisory/` subdirectory on the local machine.
+"""
