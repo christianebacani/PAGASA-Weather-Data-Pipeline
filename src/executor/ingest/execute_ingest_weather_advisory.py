83e9da7b3903f0c70c98dc3054d749ba8cd05a3e
christianebacani <christianbacani581@gmail.com>
2025-12-09 08:47:37 +0800

    refact: Docstring content of the module and the funct 'ingest_weather_advisory'

--- src/executor/ingest/execute_ingest_weather_advisory.py
+++ src/executor/ingest/execute_ingest_weather_advisory.py
@@ -1,8 +1,9 @@
-'''
-    Module for executing ingestion functions for
-    the weather advisory from the PAGASA-DOST
-    website.
-'''
+"""
+This module provides executor function for data ingestion operations.
+
+It includes function for executing ingestion functions in the
+ingest_weather_advisory module of the src/ingest package.
+"""
 from ingest.ingest_weather_advisory import create_subdir
 from ingest.ingest_weather_advisory import extract_beautiful_soup_object
 from ingest.ingest_weather_advisory import extract_weather_advisory
@@ -10,13 +11,13 @@ from ingest.ingest_weather_advisory import save_weather_advisory_to_json
 
 def ingest_weather_advisory(
 ) -> None:
-    '''
-        Ingests the weather advisory
-        from the PAGASA-DOST website
-        by executing all functions in
-        the ingest_weather_advisory module
-        of the src/ingest package.
-    '''
+    """
+    Ingests the weather advisory
+    from the PAGASA-DOST website
+    by executing all functions in
+    the ingest_weather_advisory module
+    of the src/ingest package.
+    """
     # Run all functions to ingest weather advisory data
     create_subdir()
     soup = extract_beautiful_soup_object(
