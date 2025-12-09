e1f09a2c1aaeae8da7922e9b188ff5b58313c8ed
christianebacani <christianbacani581@gmail.com>
2025-12-09 08:19:04 +0800

    refact: Content of the docstring for the funct 'create_subdir'

--- src/etl/extract/extract_weather_outlook_for_ph_cities.py
+++ src/etl/extract/extract_weather_outlook_for_ph_cities.py
@@ -9,12 +9,10 @@ import os
 def create_subdir(
 ) -> None:
     '''
-        Creates the
-        data/stage/weather_outlook_for_ph_cities/
-        subdirectory to store CSV files for
-        weather outlook for selected Philippine
-        cities from the data/raw subdirectory
-        on the local machine.
+    Creates the data/stage/weather_outlook_for_ph_cities/
+    subdirectory to store CSV files for weather outlook
+    for selected Philippine cities from the data/raw
+    subdirectory on the local machine.
     '''
     # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/stage/weather_outlook_for_ph_cities'):
