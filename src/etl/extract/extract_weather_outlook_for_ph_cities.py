7af4917bd769fed42a8e46f2f88443ad0fb1bb84
christianebacani <christianbacani581@gmail.com>
2025-12-09 13:58:02 +0800

    fix: Whitespace issues in the content of the funct 'create_subdir'

--- src/etl/extract/extract_weather_outlook_for_ph_cities.py
+++ src/etl/extract/extract_weather_outlook_for_ph_cities.py
@@ -12,7 +12,7 @@ def create_subdir(
     Creates the data/stage/weather_outlook_for_ph_cities/
     subdirectory to store CSV files for weather outlook
     for selected Philippine cities from the data/raw
-    subdirectory on the local machine.
+    subdirectoryon the local machine.
     '''
     # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
     if not os.path.exists('data/stage/weather_outlook_for_ph_cities'):
