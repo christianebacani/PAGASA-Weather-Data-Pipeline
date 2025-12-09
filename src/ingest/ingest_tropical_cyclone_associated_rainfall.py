8151625ded5981afb6be01435d39ec911b251bd8
christianebacani <christianbacani581@gmail.com>
2025-12-09 13:44:12 +0800

    refact: Docstring content of the funct 'create_subdir'

--- src/ingest/ingest_tropical_cyclone_associated_rainfall.py
+++ src/ingest/ingest_tropical_cyclone_associated_rainfall.py
@@ -17,13 +17,12 @@ from bs4 import BeautifulSoup
 
 def create_subdir(
 ) -> None:
-    '''
-        Creates the
-        data/raw/tropical_cyclone_associated_rainfall/
-        subdirectory to store JSON files for
-        tropical cyclone associated rainfall from the
-        PAGASA-DOST website.
-    '''
+    """
+    Create the `data/raw/tropical_cyclone_associated_rainfall` subdirectory to store JSON files.
+
+    This subdirectory holds the tropical cyclone associated rainfall data ingested from the
+    PAGASA-DOST website.
+    """
     # Create the data/raw/tropical_cyclone_associated_rainfall/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/tropical_cyclone_associated_rainfall'):
         os.makedirs('data/raw/tropical_cyclone_associated_rainfall')
