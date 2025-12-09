805da826b36794f7c36c938c6c982cf78e7c5586
christianebacani <christianbacani581@gmail.com>
2025-12-09 08:20:23 +0800

    refact: Docstring content of the funct 'generate_logs'

--- src/logs/logs.py
+++ src/logs/logs.py
@@ -24,13 +24,15 @@ def generate_logs(
     log_message: str
 ) -> None:
     '''
-        Generates logs for ETL
-        pipeline jobs that process
-        data from the PAGASA-DOST
-        website.
+    Generates logs for ETL
+    pipeline jobs that process
+    data from the PAGASA-DOST
+    website.
 
-        :param log_message: The message to log during ETL pipeline execution
-        :type log_message: str
+    :param log_message: The message
+        to log during ETL pipeline
+        execution
+    :type log_message: str
     '''
     format = '%Y-%m-%d %H:%M:%S' # Format: YYYY-MM-DD HH:MM:SS
     now = datetime.now()
