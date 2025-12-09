2f429df31009b97995fce0a624b20a440fd2fe97
christianebacani <christianbacani581@gmail.com>
2025-12-09 08:48:49 +0800

    refact: Docstring content of the module and it's funct 'ingest_tropical_cyclone_Advisory'

--- src/executor/ingest/execute_ingest_tropical_cyclone_advisory.py
+++ src/executor/ingest/execute_ingest_tropical_cyclone_advisory.py
@@ -1,15 +1,16 @@
-'''
-    Module for executing ingestion functions for
-    the tropical cyclone advisory from the PAGASA-
-    DOST webite.
-'''
+"""
+This module provides executor function for data ingestion operations.
+
+It includes function for executing ingestion functions in the
+ingest_tropical_cyclone_advisory module of the src/ingest package.
+"""
 
 def ingest_tropical_cyclone_advisory(
 ) -> None:
-    '''
-        Ingests the tropical cyclone
-        advisory from the PAGASA-DOST
-        website by executing all functions
-        in the ingest_tropical_cyclone_advisory module
-        of the src/ingest package.
-    '''
+    """
+    Ingests the tropical cyclone
+    advisory from the PAGASA-DOST
+    website by executing all functions
+    in the ingest_tropical_cyclone_advisory module
+    of the src/ingest package.
+    """
