2648aaac930703d7dea200b7ca8697c82aedd303
christianebacani <christianbacani581@gmail.com>
2025-12-10 12:02:53 +0800

    feat: New module created

---
+++ src/executor/extract/execute_extract_weather_outlook_for_ph_tourist_areas.py
@@ -0,0 +1,7 @@
+"""
+Execute extraction functions for weather outlook for
+selected Philippine tourist areas data.
+
+This module runs all extraction tasks on files located in the `data/raw/weather_outlook_for_ph_tourist_areas`
+subdirectory on the local machine, serving as the entry point for the daily extraction workflow.
+"""
