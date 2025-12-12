1598f01478e31504d05f36eadef700ff245bd860
christianebacani <christianbacani581@gmail.com>
2025-12-12 16:31:41 +0800

    feat: New function `extract_beautiful_soup_object`

--- src/ingest/ingest_tropical_cyclone_advisory.py
+++ src/ingest/ingest_tropical_cyclone_advisory.py
@@ -25,3 +25,25 @@ def create_subdir(
     # Create the data/raw/tropical_cyclone_advisory/ subdirectory if it doesn't exist
     if not os.path.exists('data/raw/tropical_cyclone_advisory'):
         os.makedirs('data/raw/tropical_cyclone_advisory')
+
+def extract_beautiful_soup_object(
+        url: str
+) -> BeautifulSoup | None:
+    """
+    Extract the BeautifulSoup object from the tropical cyclone advisory page.
+
+    :param url: URL of the PAGASA-DOST tropical cyclone advisory page.
+    :type url: str
+
+    :return: BeautifulSoup object for navigating the page, or None if extraction fails
+    :rtype: BeautifulSoup | None
+    """
+    response = requests.get(url)
+
+    # We need to check if the status code of the response for the request is unsuccessful
+    if response.status_code != 200:
+        return None
+
+    # Parse as a BeautifulSoup object
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
