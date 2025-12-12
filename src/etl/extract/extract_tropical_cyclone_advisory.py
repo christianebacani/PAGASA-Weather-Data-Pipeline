745fe46dd01723858eb39421580c0fd117c707ea
christianebacani <christianbacani581@gmail.com>
2025-12-12 16:07:50 +0800

    feat: New module `extract_tropical_cyclone_advisory.py`

---
+++ src/etl/extract/extract_tropical_cyclone_advisory.py
@@ -0,0 +1,14 @@
+"""
+Extract and stage tropical cyclone advisory from the `data/raw/tropical_cyclone_advisory`
+subdirectory on the local machine.
+
+This module provides functions to parse JSON files from the
+`data/raw/tropical_cyclone_advisory` subdirectory and convert them into structured DataFrame
+objects, including:
+
+- TBD
+
+Parsed DataFrames are staged as CSV files in the
+`data/stage/tropical_cyclone_advisory/` subdirectory
+on the local machine for further processing.
+"""
