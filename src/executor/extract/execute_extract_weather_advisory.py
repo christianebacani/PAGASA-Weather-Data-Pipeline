8eb2a5454a2db4b6f56ad75ee11f073794eff6c6
christianebacani <christianbacani581@gmail.com>
2025-12-12 16:03:37 +0800

    feat: New function to import `parse_weather_advisory_to_dataframe`

--- src/executor/extract/execute_extract_weather_advisory.py
+++ src/executor/extract/execute_extract_weather_advisory.py
@@ -5,6 +5,7 @@ This module runs all extraction tasks on files located in the `data/raw/weather_
 subdirectory on the local machine, serving as the entry point for the daily extraction workflow.
 """
 from etl.extract.extract_weather_advisory import create_subdir
+from etl.extract.extract_weather_advisory import parse_weather_advisory_to_dataframe
 
 def extract_weather_advisory(
 ) -> None:
@@ -19,3 +20,7 @@ def extract_weather_advisory(
     """
     # Run all functiosn to extract weather advisory data
     create_subdir()
+
+    parse_weather_advisory_to_dataframe(
+        'data/raw/weather_advisory/weather_advisory.json'
+    )
