0d32471b43a0da36ead5ecab8c513972d3315a4f
christianebacani <christianbacani581@gmail.com>
2025-12-12 15:42:11 +0800

    feat: New function `create_subdir`

--- src/etl/extract/extract_weather_advisory.py
+++ src/etl/extract/extract_weather_advisory.py
@@ -12,3 +12,17 @@ Parsed DataFrames are staged as CSV files in the
 `data/stage/weather_advisory/` subdirectory
 on the local machine for further processing.
 """
+import pandas as pd
+import os
+
+def create_subdir(
+) -> None:
+    """
+    Create the `data/stage/weather_advisory` subdirectory to store CSV files.
+
+    This subdirectory stores the weatehr advisory data parsed from JSON files located
+    in the `data/raw/weather_advisory` subdirectory on the local machine.
+    """
+    # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
+    if not os.path.exists('data/stage/weather_advisory'):
+        os.makedirs('data/stage/weather_advisory')
