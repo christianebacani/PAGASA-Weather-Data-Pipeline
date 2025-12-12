41e6430c8352463a2999a973770720bbaf97632c
christianebacani <christianbacani581@gmail.com>
2025-12-12 16:53:39 +0800

    feat: New module `extract_tropical_cyclone_associated_rainfall.py`

---
+++ src/etl/extract/extract_tropical_cyclone_associated_rainfall.py
@@ -0,0 +1,14 @@
+"""
+Extract and stage tropical cyclone associated rainfall from the `data/raw/tropical_cyclone_associated_rainfall`
+subdirectory on the local machine.
+
+This module provides functions to parse JSON files from the
+`data/raw/tropical_cyclone_associated_rainfall` subdirectory and convert them into structured DataFrame
+objects, including:
+
+- Tropical cyclone associated rainfall image source
+
+Parsed DataFrames are staged as CSV files in the
+`data/stage/tropical_cyclone_associated_rainfall/` subdirectory
+on the local machine for further processing.
+"""
