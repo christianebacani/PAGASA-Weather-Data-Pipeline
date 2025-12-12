1b73a9effdbe3a250676379f252280cfc30b21d0
christianebacani <christianbacani581@gmail.com>
2025-12-12 17:04:31 +0800

    feat: New module `execute_extract_tropical_cyclone_associated_rainfall.py`

---
+++ src/executor/extract/execute_extract_tropical_cyclone_associated_rainfall.py
@@ -0,0 +1,6 @@
+"""
+Execute extraction functions for tropical cyclone associated rainfall data
+
+This module runs all extraction tasks on files located in the `data/raw/tropical_cyclone_associated_rainfall`
+subdirectory on the local machine, serving as the entry point for the daily extraction workflow.
+"""
