cee51dd98c91e22fb41e2ca859cd85cb1df0676e
christianebacani <christianbacani581@gmail.com>
2025-12-14 14:13:53 +0800

    feat: New module `transform_daily_weather_forecast.py` with a function `create_subdir`

---
+++ src/etl/transform/transform_daily_weather_forecast.py
@@ -0,0 +1,36 @@
+"""
+Transform staged daily weather forecast data from the
+`data/stage/daily_weather_forecast/` subdirectory on the local machine.
+
+This module provides functions to clean, normalize, and transform
+staged CSV files from the `data/stage/daily_weather_forecast/`
+subdirectory into analysis-ready DataFrame objects, including:
+
+- Issued datetime
+- Synopsis
+- Tropical cyclone information
+- Forecast weather conditions
+- Forecast wind and coastal water condition data
+- Temperature and relative humidity values
+
+Transformed DataFrames are saved as CSV files in the
+`data/processed/daily_weather_forecast/` subdirectory
+on the local machine for further processing before
+analysis and consumption.
+"""
+import pandas as pd
+import os
+
+def create_subdir(
+) -> None:
+    """
+    Create the `data/processed/daily_weather_forecast` subdirectory to store
+    transformed or processed CSV files.
+
+    This subdirectory stores the daily weather forecast data, transformed or processed
+    from CSV files located in the `data/stage/daily_weather_forecast` subdirectory on the
+    local machine.
+    """
+    # Create the data/processed/daily_weather_forecast/ subdirectory if it doesn't exist
+    if not os.path.exists('data/processed/daily_weather_forecast'):
+        os.makedirs('data/processed/daily_weather_forecast')
