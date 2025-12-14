6faa9fe5072a65932ab458d96ecc4d40b1baf0ac
christianebacani <christianbacani581@gmail.com>
2025-12-14 14:08:34 +0800

    fix: Content of the docstring for the function `create_subdir`

--- src/etl/extract/extract_tropical_cyclone_associated_rainfall.py
+++ src/etl/extract/extract_tropical_cyclone_associated_rainfall.py
@@ -20,7 +20,7 @@ def create_subdir(
     """
     Create the `data/stage/tropical_cyclone_associated_rainfall` subdirectory to store CSV files.
 
-    This subdirectory stores the tropical cyclone associated rainfall data parsed from JSON files
+    This subdirectory stores the tropical cyclone associated rainfall data, parsed from JSON files
     located in the `data/raw/tropical_cyclone_associated_rainfall` subdirectory on the local machine.
     """
     # Create the data/stage/tropical_cyclone_associated_rainfall/ subdirectory if it doesn't exist
