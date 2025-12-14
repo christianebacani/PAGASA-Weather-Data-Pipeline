2f625633e7ad0c325651dc718e62ebfc7ab14236
christianebacani <christianbacani581@gmail.com>
2025-12-14 14:20:39 +0800

    feat: New module `execute_transform_daily_weather_forecast.py` with a function `transform_daily_weather_forecast`

---
+++ src/executor/transform/execute_transform_daily_weather_forecast.py
@@ -0,0 +1,21 @@
+"""
+Execute transformation functions for daily weather forecast data
+
+This module runs all transformation tasks on files located in the `data/stage/daily_weather_forecast`
+subdirectory on the local machine, serving as the entry point for the daily transformation workflow.
+"""
+from etl.transform.transform_daily_weather_forecast import create_subdir
+
+def transform_daily_weather_forecast(
+) -> None:
+    """
+    Transform the daily weather forecast from the
+    `data/stage/daily_weather_forecast` subdirectory
+    on the local machine.
+
+    This function executes all transformation functions
+    in the `transform_daily_weather_forecast` module of the
+    `src.etl.transform` package.
+    """
+    # Run all functions to extract tropical cyclone associated rainfall data
+    create_subdir()
