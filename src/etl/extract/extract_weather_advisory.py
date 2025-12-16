7110461aa2e092d0afe04bb175f47ad2742423a5
christianebacani <christianbacani581@gmail.com>
2025-12-16 23:46:22 +0800

    fix: Content of the docstring

--- src/etl/extract/extract_weather_advisory.py
+++ src/etl/extract/extract_weather_advisory.py
@@ -1,5 +1,5 @@
 """
-Extract and stage weather advisory from the `data/raw/weather_advisory` subdirectory
+Extract weather advisory from the `data/raw/weather_advisory` subdirectory
 on the local machine.
 
 This module provides functions to parse JSON files from the
@@ -8,7 +8,7 @@ objects, including:
 
 - Weather advisory source URL or content
 
-Parsed DataFrames are staged as CSV files in the
+Parsed DataFrames are saved as CSV files in the
 `data/stage/weather_advisory/` subdirectory
 on the local machine for further processing.
 """
