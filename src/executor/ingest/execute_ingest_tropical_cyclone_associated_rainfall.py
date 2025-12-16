139598d7dfdaae5d890191c0eb435e80e4c3a109
christianebacani <christianbacani581@gmail.com>
2025-12-16 23:21:56 +0800

    fix: Function names

--- src/executor/ingest/execute_ingest_tropical_cyclone_associated_rainfall.py
+++ src/executor/ingest/execute_ingest_tropical_cyclone_associated_rainfall.py
@@ -6,10 +6,10 @@ This module executes ingestion functions in the
 `src.ingest` package.
 """
 from ingest.ingest_tropical_cyclone_associated_rainfall import create_subdir
-from ingest.ingest_tropical_cyclone_associated_rainfall import extract_beautiful_soup_object
-from ingest.ingest_tropical_cyclone_associated_rainfall import extract_tc_assoc_rainfall_tag
-from ingest.ingest_tropical_cyclone_associated_rainfall import extract_tc_assoc_rainfall_image_sources
-from ingest.ingest_tropical_cyclone_associated_rainfall import save_tc_assoc_rainfall_image_sources_to_json
+from ingest.ingest_tropical_cyclone_associated_rainfall import ingest_beautiful_soup_object
+from ingest.ingest_tropical_cyclone_associated_rainfall import ingest_tc_assoc_rainfall_tag
+from ingest.ingest_tropical_cyclone_associated_rainfall import ingest_tc_assoc_rainfall_image_sources
+from ingest.ingest_tropical_cyclone_associated_rainfall import save_tc_assoc_rainfall_image_sources_to_raw_subdir
 
 def ingest_tropical_cyclone_associated_rainfall(
 ) -> None:
@@ -21,80 +21,80 @@ def ingest_tropical_cyclone_associated_rainfall(
     """
     # Run all functions to ingest tropical cyclone associated rainfall data
     create_subdir()
-    soup = extract_beautiful_soup_object(
+    soup = ingest_beautiful_soup_object(
         'https://www.pagasa.dost.gov.ph/climate/tropical-cyclone-associated-rainfall'
     )
 
-    tc_assoc_rainfall_tag = extract_tc_assoc_rainfall_tag(soup)
+    tc_assoc_rainfall_tag = ingest_tc_assoc_rainfall_tag(soup)
 
-    tc_assoc_rainfall_image_sources_2025 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2025 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2025
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2025,
         2025
     )
 
-    tc_assoc_rainfall_image_sources_2024 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2024 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2024
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2024,
         2024
     )
 
-    tc_assoc_rainfall_image_sources_2023 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2023 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2023
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2023,
         2023
     )
 
-    tc_assoc_rainfall_image_sources_2022 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2022 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2022
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2022,
         2022
     )
 
-    tc_assoc_rainfall_image_sources_2021 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2021 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2021
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2021,
         2021
     )
 
-    tc_assoc_rainfall_image_sources_2020 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2020 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2020
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2020,
         2020
     )
 
-    tc_assoc_rainfall_image_sources_2019 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2019 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2019
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2019,
         2019
     )
 
-    tc_assoc_rainfall_image_sources_2018 = extract_tc_assoc_rainfall_image_sources(
+    tc_assoc_rainfall_image_sources_2018 = ingest_tc_assoc_rainfall_image_sources(
         tc_assoc_rainfall_tag,
         2018
     )
-    save_tc_assoc_rainfall_image_sources_to_json(
+    save_tc_assoc_rainfall_image_sources_to_raw_subdir(
         tc_assoc_rainfall_image_sources_2018,
         2018
     )
