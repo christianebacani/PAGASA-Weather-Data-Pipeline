730279c994b9e98eb22567c0778335d8e481c64b
christianebacani <christianbacani581@gmail.com>
2025-12-16 23:34:33 +0800

    fix: Function names

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -7,17 +7,17 @@ daily extraction workflow.
 """
 from etl.extract.extract_daily_weather_forecast import create_subdir
 from etl.extract.extract_daily_weather_forecast import parse_issued_datetime_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_issued_datetime_dataframe
+from etl.extract.extract_daily_weather_forecast import save_issued_datetime_dataframe_to_stage_subdir
 from etl.extract.extract_daily_weather_forecast import parse_synopsis_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_synopsis_dataframe
+from etl.extract.extract_daily_weather_forecast import save_synopsis_dataframe_to_stage_subdir
 from etl.extract.extract_daily_weather_forecast import parse_tc_information_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_tc_information_dataframe
+from etl.extract.extract_daily_weather_forecast import save_tc_information_dataframe_to_stage_subdir
 from etl.extract.extract_daily_weather_forecast import parse_forecast_weather_conditions_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_forecast_weather_conditions_dataframe
+from etl.extract.extract_daily_weather_forecast import save_forecast_weather_conditions_dataframe_to_stage_subdir
 from etl.extract.extract_daily_weather_forecast import parse_forecast_wind_and_coastal_water_conditions_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_forecast_wind_and_coastal_water_conditions_dataframe
+from etl.extract.extract_daily_weather_forecast import save_forecast_wind_and_coastal_water_conditions_dataframe_to_stage_subdir
 from etl.extract.extract_daily_weather_forecast import parse_temperature_and_relative_humidity_to_dataframe
-from etl.extract.extract_daily_weather_forecast import stage_temperature_and_relative_humidity_dataframe
+from etl.extract.extract_daily_weather_forecast import save_temperature_and_relative_humidity_dataframe_to_stage_subdir
 
 def extract_daily_weather_forecast(
 ) -> None:
@@ -35,41 +35,41 @@ def extract_daily_weather_forecast(
     issued_datetime_dataframe = parse_issued_datetime_to_dataframe(
         'data/raw/daily_weather_forecast/issued_datetime.json'
     )
-    stage_issued_datetime_dataframe(
+    save_issued_datetime_dataframe_to_stage_subdir(
         issued_datetime_dataframe
     )
 
     synopsis_dataframe = parse_synopsis_to_dataframe(
         'data/raw/daily_weather_forecast/synopsis.json'
     )
-    stage_synopsis_dataframe(
+    save_synopsis_dataframe_to_stage_subdir(
         synopsis_dataframe
     )
 
     tc_information_dataframe = parse_tc_information_to_dataframe(
         'data/raw/daily_weather_forecast/tropical_cyclone_information.json'
     )
-    stage_tc_information_dataframe(
+    save_tc_information_dataframe_to_stage_subdir(
         tc_information_dataframe
     )
 
     forecast_weather_conditions_dataframe = parse_forecast_weather_conditions_to_dataframe(
         'data/raw/daily_weather_forecast/forecast_weather_conditions.json'
     )
-    stage_forecast_weather_conditions_dataframe(
+    save_forecast_weather_conditions_dataframe_to_stage_subdir(
         forecast_weather_conditions_dataframe
     )
 
     forecast_wind_and_coastal_water_conditions_dataframe = parse_forecast_wind_and_coastal_water_conditions_to_dataframe(
         'data/raw/daily_weather_forecast/forecast_wind_and_coastal_water_conditions.json'
     )
-    stage_forecast_wind_and_coastal_water_conditions_dataframe(
+    save_forecast_wind_and_coastal_water_conditions_dataframe_to_stage_subdir(
         forecast_wind_and_coastal_water_conditions_dataframe
     )
 
     temperature_and_relative_humidity_dataframe = parse_temperature_and_relative_humidity_to_dataframe(
         'data/raw/daily_weather_forecast/temperature_and_relative_humidity.json'
     )
-    stage_temperature_and_relative_humidity_dataframe(
+    save_temperature_and_relative_humidity_dataframe_to_stage_subdir(
         temperature_and_relative_humidity_dataframe
     )
