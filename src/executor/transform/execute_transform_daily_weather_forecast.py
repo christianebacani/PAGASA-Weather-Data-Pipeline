2b0110e25be76844071a5555fa585742f6743a92
christianebacani <christianbacani581@gmail.com>
2025-12-18 22:58:37 +0800

    feat: Function `save_forecast_weather_conditions_with_issued_datetime_to_processed_subdir`

--- src/executor/transform/execute_transform_daily_weather_forecast.py
+++ src/executor/transform/execute_transform_daily_weather_forecast.py
@@ -13,6 +13,7 @@ from etl.transform.transform_daily_weather_forecast import enrich_synopsis_with_
 from etl.transform.transform_daily_weather_forecast import save_synopsis_with_issued_datetime_to_processed_subdir
 from etl.transform.transform_daily_weather_forecast import transform_forecast_weather_conditions_dataframe
 from etl.transform.transform_daily_weather_forecast import enrich_forecast_weather_conditions_with_issued_datetime
+from etl.transform.transform_daily_weather_forecast import save_forecast_weather_conditions_with_issued_datetime_to_processed_subdir
 
 def transform_daily_weather_forecast(
 ) -> None:
@@ -57,4 +58,7 @@ def transform_daily_weather_forecast(
     forecast_weather_conditions_with_issued_datetime_dataframe = enrich_forecast_weather_conditions_with_issued_datetime(
         transformed_forecast_weather_conditions_dataframe,
         transformed_issued_datetime_dataframe
+    )
+    save_forecast_weather_conditions_with_issued_datetime_to_processed_subdir(
+        forecast_weather_conditions_with_issued_datetime_dataframe
     )
