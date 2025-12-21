e1c60b341daddab06d4919bceb985c02ef9aeba1
christianebacani <christianbacani581@gmail.com>
2025-12-21 23:05:05 +0800

    refact: Structure of parameters when running the function and its names

--- src/executor/ingest/execute_ingest_daily_weather_forecast.py
+++ src/executor/ingest/execute_ingest_daily_weather_forecast.py
@@ -7,17 +7,17 @@ This module executes ingestion functions in the
 from ingest.ingest_daily_weather_forecast import create_subdir
 from ingest.ingest_daily_weather_forecast import ingest_beautiful_soup_object
 from ingest.ingest_daily_weather_forecast import ingest_issued_datetime
-from ingest.ingest_daily_weather_forecast import save_issued_datetime_to_raw_subdir
+from ingest.ingest_daily_weather_forecast import save_ingested_issued_datetime
 from ingest.ingest_daily_weather_forecast import ingest_synopsis
-from ingest.ingest_daily_weather_forecast import save_synopsis_to_raw_subdir
-from ingest.ingest_daily_weather_forecast import ingest_tc_information
-from ingest.ingest_daily_weather_forecast import save_tc_information_to_raw_subdir
+from ingest.ingest_daily_weather_forecast import save_ingested_synopsis
+from ingest.ingest_daily_weather_forecast import ingest_tropical_cyclone_information
+from ingest.ingest_daily_weather_forecast import save_ingested_tropical_cyclone_information
 from ingest.ingest_daily_weather_forecast import ingest_forecast_weather_conditions
-from ingest.ingest_daily_weather_forecast import save_forecast_weather_conditions_to_raw_subdir
+from ingest.ingest_daily_weather_forecast import save_ingested_forecast_weather_conditions
 from ingest.ingest_daily_weather_forecast import ingest_forecast_wind_and_coastal_water_conditions
-from ingest.ingest_daily_weather_forecast import save_forecast_wind_and_coastal_water_conditions_to_raw_subdir
+from ingest.ingest_daily_weather_forecast import save_ingested_forecast_wind_and_coastal_water_conditions
 from ingest.ingest_daily_weather_forecast import ingest_temperature_and_relative_humidity
-from ingest.ingest_daily_weather_forecast import save_temperature_and_relative_humidity_to_raw_subdir
+from ingest.ingest_daily_weather_forecast import save_ingested_temperature_and_relative_humidity
 
 def ingest_daily_weather_forecast(
 ) -> None:
@@ -34,20 +34,44 @@ def ingest_daily_weather_forecast(
         'https://www.pagasa.dost.gov.ph/weather#daily-weather-forecast'
     )
 
-    issued_datetime = ingest_issued_datetime(soup)
-    save_issued_datetime_to_raw_subdir(issued_datetime)
+    issued_datetime = ingest_issued_datetime(
+        soup
+    )
+    save_ingested_issued_datetime(
+        issued_datetime
+    )
 
-    synopsis = ingest_synopsis(soup)
-    save_synopsis_to_raw_subdir(synopsis)
+    synopsis = ingest_synopsis(
+        soup
+    )
+    save_ingested_synopsis(
+        synopsis
+    )
 
-    tc_information = ingest_tc_information(soup)
-    save_tc_information_to_raw_subdir(tc_information)
+    tropical_cyclone_information = ingest_tropical_cyclone_information(
+        soup
+    )
+    save_ingested_tropical_cyclone_information(
+        tropical_cyclone_information
+    )
 
-    forecast_weather_conditions = ingest_forecast_weather_conditions(soup)
-    save_forecast_weather_conditions_to_raw_subdir(forecast_weather_conditions)
+    forecast_weather_conditions = ingest_forecast_weather_conditions(
+        soup
+    )
+    save_ingested_forecast_weather_conditions(
+        forecast_weather_conditions
+    )
 
-    forecast_wind_and_coastal_water_conditions = ingest_forecast_wind_and_coastal_water_conditions(soup)
-    save_forecast_wind_and_coastal_water_conditions_to_raw_subdir(forecast_wind_and_coastal_water_conditions)
-    
-    temperature_and_relative_humidity = ingest_temperature_and_relative_humidity(soup)
-    save_temperature_and_relative_humidity_to_raw_subdir(temperature_and_relative_humidity)
+    forecast_wind_and_coastal_water_conditions = ingest_forecast_wind_and_coastal_water_conditions(
+        soup
+    )
+    save_ingested_forecast_wind_and_coastal_water_conditions(
+        forecast_wind_and_coastal_water_conditions
+    )
+
+    temperature_and_relative_humidity = ingest_temperature_and_relative_humidity(
+        soup
+    )
+    save_ingested_temperature_and_relative_humidity(
+        temperature_and_relative_humidity
+    )
