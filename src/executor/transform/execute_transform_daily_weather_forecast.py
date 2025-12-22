0b5d35153d7a7caf76d10c535623e73332795385
christianebacani <christianbacani581@gmail.com>
2025-12-22 09:56:43 +0800

    refact: Docstring content of the top-level module and function `transform_daily_weather_forecast`

--- src/executor/transform/execute_transform_daily_weather_forecast.py
+++ src/executor/transform/execute_transform_daily_weather_forecast.py
@@ -1,7 +1,7 @@
 """
 Execute transformation functions for daily weather forecast data
 
-This module runs all transformation tasks on files located in the `data/stage/daily_weather_forecast`
+This module runs all transformation tasks on files located in the `data/stage/daily_weather_forecast/`
 subdirectory on the local machine, serving as the entry point for the daily transformation workflow.
 """
 import pandas as pd
@@ -15,7 +15,7 @@ def transform_daily_weather_forecast(
 ) -> None:
     """
     Transform the daily weather forecast from the
-    `data/stage/daily_weather_forecast` subdirectory
+    `data/stage/daily_weather_forecast/` subdirectory
     on the local machine.
 
     This function executes all transformation functions
