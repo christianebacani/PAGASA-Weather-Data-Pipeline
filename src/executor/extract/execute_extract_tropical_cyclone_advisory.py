e27b767a78b5dc2fc28802f743ce0bf6c04fa5fb
christianebacani <christianbacani581@gmail.com>
2025-12-22 09:58:45 +0800

    refact: Docstring content of the top-level module and function `extract_tropical_cyclone_advisory`

--- src/executor/extract/execute_extract_tropical_cyclone_advisory.py
+++ src/executor/extract/execute_extract_tropical_cyclone_advisory.py
@@ -1,7 +1,7 @@
 """
 Execute extraction functions for tropical cyclone advisory data
 
-This module runs all extraction tasks on files located in the `data/raw/tropical_cyclone_advisory`
+This module runs all extraction tasks on files located in the `data/raw/tropical_cyclone_advisory/`
 subdirectory on the local machine, serving as the entry point for the daily extraction workflow.
 """
 from etl.extract.extract_tropical_cyclone_advisory import create_subdir
@@ -10,7 +10,7 @@ def extract_tropical_cyclone_advisory(
 ) -> None:
     """
     Extract the tropical cyclone advisory from the
-    `data/raw/tropical_cyclone_advisory` subdirectory
+    `data/raw/tropical_cyclone_advisory/` subdirectory
     on the local machine.
 
     This function executes all extraction functions in the
