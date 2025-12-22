e22302ffd3cb43c8f868b27a36ff7775e27e287d
christianebacani <christianbacani581@gmail.com>
2025-12-22 09:46:27 +0800

    refact: Docstring content of the function `create_subdir`

--- src/etl/extract/extract_weather_outlook_for_ph_cities.py
+++ src/etl/extract/extract_weather_outlook_for_ph_cities.py
@@ -23,7 +23,7 @@ def create_subdir(
     Create the `data/stage/weather_outlook_for_ph_cities/` subdirectory to store CSV files.
 
     This subdirectory stores the staged weather outlook data for selected Philippine cities,
-    parsed from JSON files located in the `data/raw/weather_outlook_for_ph_cities` subdirectory
+    parsed from JSON files located in the `data/raw/weather_outlook_for_ph_cities/` subdirectory
     on the local machine.
     """
     # Create the data/stage/weather_outlook_for_ph_cities/ subdirectory if it doesn't exist
