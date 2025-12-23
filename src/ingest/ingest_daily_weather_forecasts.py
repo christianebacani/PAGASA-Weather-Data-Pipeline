0c4a16a4c73709585dd943237d45078758c45687
christianebacani <christianbacani581@gmail.com>
2025-12-23 23:27:05 +0800

    refact: Docstring content of the function `create_subdir`

--- src/ingest/ingest_daily_weather_forecasts.py
+++ src/ingest/ingest_daily_weather_forecasts.py
@@ -5,5 +5,7 @@ Docstring of the ingest_daily_weather_forecasts.py
 def create_subdir(
 ) -> None:
     """
-    Docstring for the function create_subdir
+    Creates a subdirectory path `data/raw/daily_weather_forecasts/`
+    to store ingested data as a JSON file from the daily weather
+    forecast page of PAGAASA-DOST website.
     """
