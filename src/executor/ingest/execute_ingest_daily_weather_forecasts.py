8fa01b80d6cf3a37ebf2efbffad4a82f65d2fc86
christianebacani <christianbacani581@gmail.com>
2025-12-23 23:48:10 +0800

    feat: New function `ingest_daily_weather_forecasts`

--- src/executor/ingest/execute_ingest_daily_weather_forecasts.py
+++ src/executor/ingest/execute_ingest_daily_weather_forecasts.py
@@ -1,3 +1,12 @@
 """
 Docstring for executor.ingest.execute_ingest_daily_weather_forecasts
 """
+
+def ingest_daily_weather_forecasts(
+) -> None:
+    """
+    Executes the function in the
+    `src.ingest.ingest_daily_weather_forecasts.py`
+    module to ingest the data from the daily
+    weather forecast page of PAGASA-DOST website.
+    """
