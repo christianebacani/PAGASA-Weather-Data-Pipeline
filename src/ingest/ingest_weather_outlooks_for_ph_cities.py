b9703be43cbe14ef579b25876bcc5cafab4f9171
christianebacani <christianbacani581@gmail.com>
2025-12-26 12:12:38 +0800

    feat: New function `ingest_and_parse_from_url`

--- src/ingest/ingest_weather_outlooks_for_ph_cities.py
+++ src/ingest/ingest_weather_outlooks_for_ph_cities.py
@@ -15,3 +15,9 @@ def create_subdir(
     """
     if not os.path.exists('data/raw/weather_outlooks_for_ph_cities'):
         os.makedirs('data/raw/weather_outlooks_for_ph_cities')
+
+def ingest_and_parse_from_url(
+        url: str
+) -> BeautifulSoup | None:
+    """
+    """
