98e9d77106d057f77e718ef6841f40e5928324af
christianebacani <christianbacani581@gmail.com>
2025-12-26 11:58:45 +0800

    refact: Content of the function `create_subdir`

--- src/ingest/ingest_weather_outlooks_for_ph_cities.py
+++ src/ingest/ingest_weather_outlooks_for_ph_cities.py
@@ -10,7 +10,7 @@ def create_subdir(
 ) -> None:
     """
     Creates the subdirectory path `data/raw/weather_outlooks_for_ph_cities/`
-    for ingested data from the weather outlook for selected Philippine Cities
+    for ingested data from the weather outlook for selected Philippine cities
     page of PAGASA-DOST website.
     """
     if not os.path.exists('data/raw/weather_outlooks_for_ph_cities'):
