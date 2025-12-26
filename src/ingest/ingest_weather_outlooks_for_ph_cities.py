c73295cfe4e9ea2cd5f9eed4a6dafc25b1871e91
christianebacani <christianbacani581@gmail.com>
2025-12-26 11:31:36 +0800

    feat: New module `ingest_weather_outlooks_for_ph_cities.py` with a function `create_subdir` on its content

---
+++ src/ingest/ingest_weather_outlooks_for_ph_cities.py
@@ -0,0 +1,11 @@
+"""
+Docstring for ingest.ingest_weather_outlooks_for_ph_cities
+"""
+
+def create_subdir(
+) -> None:
+    """
+    Creates the subdirectory path `data/raw/weather_outlooks_for_ph_cities/`
+    for ingested data from the weather outlook for selected Philippine Cities
+    page of PAGASA-DOST website.
+    """
