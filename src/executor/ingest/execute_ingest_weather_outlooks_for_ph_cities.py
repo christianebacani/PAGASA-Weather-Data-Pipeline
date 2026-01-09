84532c3a70c3680a2bce6b61ccf589e87499d782
christianebacani <christianbacani581@gmail.com>
2026-01-09 20:01:04 +0800

    feat: New function to run `map_ph_city_names_to_chance_of_rain_percentages`

--- src/executor/ingest/execute_ingest_weather_outlooks_for_ph_cities.py
+++ src/executor/ingest/execute_ingest_weather_outlooks_for_ph_cities.py
@@ -14,6 +14,7 @@ from ingest.ingest_weather_outlooks_for_ph_cities import map_ph_city_names_to_we
 from ingest.ingest_weather_outlooks_for_ph_cities import ingest_temperature_ranges
 from ingest.ingest_weather_outlooks_for_ph_cities import map_ph_city_names_to_temperature_ranges
 from ingest.ingest_weather_outlooks_for_ph_cities import ingest_chance_of_rain_percentages
+from ingest.ingest_weather_outlooks_for_ph_cities import map_ph_city_names_to_chance_of_rain_percentages
 
 def ingest_weather_outlooks_for_ph_cities(
 ) -> None:
@@ -69,4 +70,8 @@ def ingest_weather_outlooks_for_ph_cities(
 
     list_of_all_chance_of_rain_percentages = ingest_chance_of_rain_percentages(
         list_of_all_ph_city_tags
+    )
+    weather_outlooks_for_ph_cities = map_ph_city_names_to_chance_of_rain_percentages(
+        weather_outlooks_for_ph_cities,
+        list_of_all_chance_of_rain_percentages
     )
