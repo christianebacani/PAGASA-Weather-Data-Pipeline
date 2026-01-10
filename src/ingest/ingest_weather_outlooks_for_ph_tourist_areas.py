107d6d831f275962b66efb4f209c7b05d90d25a5
christianebacani <christianbacani581@gmail.com>
2026-01-10 15:13:35 +0800

    feat: New function `create_subdir`

--- src/ingest/ingest_weather_outlooks_for_ph_tourist_areas.py
+++ src/ingest/ingest_weather_outlooks_for_ph_tourist_areas.py
@@ -1,3 +1,11 @@
 """
 Docstring for ingest.ingest_weather_outlooks_for_ph_tourist_areas
 """
+
+def create_subdir(
+) -> None:
+    """
+    Creates the subdirectory path `data/raw/weather_outlooks_for_ph_tourist_areas/`
+    for ingested data from the weather outlook for selected Philippine tourist areas
+    page of PAGASA-DOST website.
+    """
