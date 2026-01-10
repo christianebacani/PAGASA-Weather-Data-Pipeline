0b387fd0d06ddc4810128a96ded61ae02cc0e8c7
christianebacani <christianbacani581@gmail.com>
2026-01-10 15:43:37 +0800

    feat: New function `ingest_weather_outlooks_for_ph_tourist_areas`

--- src/executor/ingest/execute_ingest_weather_outlooks_for_ph_tourist_areas.py
+++ src/executor/ingest/execute_ingest_weather_outlooks_for_ph_tourist_areas.py
@@ -1,3 +1,13 @@
 """
 Docstring for executor.ingest.execute_ingest_weather_outlooks_for_ph_tourist_areas
 """
+
+def ingest_weather_outlooks_for_ph_tourist_areas(
+) -> None:
+    """
+    Executes the function in the
+    `src.ingest.ingest_weather_outlooks_for_ph_tourist_areas.py`
+    module to ingest the data from the weather outlooks
+    for selected Philippine tourist areas page of PAGASA-DOST
+    website.
+    """
