96c16a88fd9c21c84658908535cbeda4d0f72c76
christianebacani <christianbacani581@gmail.com>
2026-01-13 23:48:52 +0800

    feat: New content of the function `ingest_weather_dates`

--- src/ingest/ingest_weather_outlooks_for_ph_tourist_areas.py
+++ src/ingest/ingest_weather_outlooks_for_ph_tourist_areas.py
@@ -216,3 +216,21 @@ def ingest_weather_dates(
             'class': 'row weather-page'
         }
     )
+    weather_outlooks_for_ph_tourist_areas_tag = div_tag_with_row_weather_page_class.find(
+        'div',
+        attrs={
+            'class': 'col-md-12 col-lg-12'
+        }
+    )
+    table_with_table_desktop_class = weather_outlooks_for_ph_tourist_areas_tag.find(
+        'table',
+        attrs={
+            'class': 'table desktop'
+        }
+    )
+    thead_tag = table_with_table_desktop_class.find(
+        'thead'
+    )
+    list_of_all_table_header_tags = thead_tag.find_all(
+        'th'
+    )[1:]
