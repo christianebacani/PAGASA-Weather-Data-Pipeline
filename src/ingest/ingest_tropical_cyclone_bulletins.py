a7ce9711dd9e2393be37316f2cf7dba8fd8dbdd0
christianebacani <christianbacani581@gmail.com>
2026-01-19 09:25:41 +0800

    feat: New function to create to ingest and parse BeautifulSoup object to use for ingestion of tropical cyclone bulletins

--- src/ingest/ingest_tropical_cyclone_bulletins.py
+++ src/ingest/ingest_tropical_cyclone_bulletins.py
@@ -15,3 +15,29 @@ def create_subdir(
     """
     if not os.path.exists('data/raw/tropical_cyclone_bulletins'):
         os.makedirs('data/raw/tropical_cyclone_bulletins')
+
+def ingest_and_parse_soup_from_url(
+        url: str
+) -> BeautifulSoup | None:
+    """
+    Ingest and parse BeautifulSoup object
+    from the URL of the tropical cyclone
+    bulletins page of the PAGASA-DOST website.
+
+    :param url: URL of the tropical cyclone
+        bulletins page to ingest and parse
+    :type url: str
+
+    :return: A BeautifulSoup object representing
+        the parsed HTML of the page, or NoneType if
+        the page does not allow scraping
+    :rtype: BeautifulSoup | None
+    """
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    return soup
