9386a1df9251ffc9ac56a683f949d80f72b358d8
christianebacani <christianbacani581@gmail.com>
2026-01-19 23:38:51 +0800

    fix: Rename the module from `ingest_tropical_cyclone_bulletins` to `ingest_tropical_cyclone_bulletin`

--- src/ingest/__init__.py
+++ src/ingest/__init__.py
@@ -2,4 +2,4 @@ from . import ingest_daily_weather_forecasts
 from . import ingest_weather_outlooks_for_ph_cities
 from . import ingest_weather_outlooks_for_ph_tourist_areas
 from . import ingest_weather_advisories
-from . import ingest_tropical_cyclone_bulletins
+from . import ingest_tropical_cyclone_bulletin
