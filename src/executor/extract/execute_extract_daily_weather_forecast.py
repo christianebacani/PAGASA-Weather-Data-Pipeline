a11b7cfdeabeca40981e6665ae2e806e40398a58
christianebacani <christianbacani581@gmail.com>
2026-01-22 23:07:10 +0800

    refact: Docstring content of the function

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -8,7 +8,7 @@ def extract_daily_weather_forecast(
     """
     Executes the function in the
     `src.etl.extract.extract_daily_weather_forecast`
-    module to extract the data from the `data/raw/daily_weather_forecasts/`
+    module to extract the data from the `data/daily_weather_forecasts/`
     subdirectory path that consist of ingested artifacts as a JSON file
     """
     connect()
