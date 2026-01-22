1d280f8a03ecf268c6e8f61c15e99ce9f3449d93
christianebacani <christianbacani581@gmail.com>
2026-01-22 09:52:15 +0800

    feat: New module

---
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -0,0 +1,14 @@
+"""
+Docstring for src.executor.extract.execute_extract_daily_weather_forecast
+"""
+from etl.extract.extract_daily_weather_forecast import connect
+
+def extract_daily_weather_forecast(
+) -> None:
+    """
+    Executes the function in the
+    `src.etl.extract.extract_daily_weather_forecast`
+    module to extract the data from the `data/raw/daily_weather_forecasts/`
+    subdirectory path that consist of ingested artifacts as a JSON file
+    """
+    connect()
