aa7418f01f5d56a4df8ccd3ec7437595bb3e713b
christianebacani <christianbacani581@gmail.com>
2026-01-22 23:41:06 +0800

    feat: New function `extract_issued_datetime`

--- src/etl/extract/extract_daily_weather_forecast.py
+++ src/etl/extract/extract_daily_weather_forecast.py
@@ -1,3 +1,18 @@
 """
 Docstring for etl.extract.extract_daily_weather_forecast
 """
+
+def extract_issued_datetime(
+        issued_datetime_filepath: str        
+) -> None:
+    """
+    Extract the ingested issued datetime of
+    daily weather forecast page as a JSON file
+    from the subdirectory path
+    `data/daily_weather_forecasts`.
+
+    :param issued_datetime_filepath: Issued
+        datetime of the daily weather forecast
+        page as a JSON file
+    :type issued_datetime_filepath: str
+    """
