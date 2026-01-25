5354c5448a484bad98bd00c3534c470bd4580e05
christianebacani <christianbacani581@gmail.com>
2026-01-26 07:12:40 +0800

    refact: Remove the function `connect` to be executed inside the content of the function `extract_daily_weather_forecast`

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -14,13 +14,6 @@ def extract_daily_weather_forecast(
     module to extract the data from the `data/raw/daily_weather_forecasts/`
     subdirectory path that consist of ingested artifacts as a JSON file
     """
-    conn = connect(
-        'username',
-        'password',
-        'account_identifier',
-        'warehouse_name'
-    )
-
     issued_datetime_dataframe = extract_issued_datetime(
         'data/raw/daily_weather_forecasts/issued_datetime.json'
     )
