e6bf8dcc66b1f6ec8f5457918ea8e3f890549d8f
christianebacani <christianbacani581@gmail.com>
2026-01-25 16:24:41 +0800

    refact: Rename the function from `transform_issued_datetime` to `clean_issued_datetime` and fix the docstring content of that function.

--- src/etl/extract/extract_daily_weather_forecast.py
+++ src/etl/extract/extract_daily_weather_forecast.py
@@ -99,43 +99,16 @@ def extract_issued_datetime(
 
     return issued_datetime_dataframe
 
-def transform_issued_datetime(
+def clean_issued_datetime(
         issued_datetime_dataframe: pd.DataFrame
 ) -> pd.DataFrame:
     """
-    Transform the issued datetime as a DataFrame object.
+    Clean the issued datetime as a DataFrame object.
 
     :param issued_datetime_dataframe: Issued datetime as
         a DataFrame object
     :type issued_datetime_dataframe: pd.DataFrame
 
-    :return: Transformed issued datetime as a DataFrame
-        object
+    :return: Cleaned issued datetime as a DataFrame object
     :rtype: DataFrame
     """
-    columns = [
-        'issued_time',
-        'issued_date'
-    ]
-    transformed_issued_datetime = pd.DataFrame(
-        columns=columns
-    )
-
-    issued_datetime = issued_datetime_dataframe['issued_datetime'][0]
-    issued_datetime = str(issued_datetime)
-    issued_datetime = issued_datetime.strip()
-    issued_datetime = issued_datetime.replace(
-        'Issued at:', ''
-    )
-    issued_time = issued_datetime.split(', ')[0]
-    issued_date = issued_datetime.split(', ')[1]
-
-    transformed_issued_datetime = pd.concat([
-        transformed_issued_datetime,
-        pd.DataFrame({
-            'issued_time': [issued_time],
-            'issued_date': [issued_date]
-        })
-    ])
-
-    return transformed_issued_datetime
