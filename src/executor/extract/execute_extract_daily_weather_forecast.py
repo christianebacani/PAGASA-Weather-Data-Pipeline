ef032e7d08933794272c49839ac86a5bd417243f
christianebacani <christianbacani581@gmail.com>
2026-01-25 14:53:07 +0800

    refact: Docstring content of the function `extract_daily_weather_forecast` and value of the parameter for the extract functions

--- src/executor/extract/execute_extract_daily_weather_forecast.py
+++ src/executor/extract/execute_extract_daily_weather_forecast.py
@@ -16,37 +16,37 @@ def extract_daily_weather_forecast(
     """
     Executes the function in the
     `src.etl.extract.extract_daily_weather_forecast`
-    module to extract the data from the `data/daily_weather_forecasts/`
+    module to extract the data from the `data/raw/daily_weather_forecasts/`
     subdirectory path that consist of ingested artifacts as a JSON file
     """
     issued_datetime_dataframe = extract_issued_datetime(
-        'data/daily_weather_forecasts/issued_datetime.json'
+        'data/raw/daily_weather_forecasts/issued_datetime.json'
     )
     transformed_issued_datetime = transform_issued_datetime(
         issued_datetime_dataframe
     )
 
     synopsis_dataframe = extract_synopsis(
-        'data/daily_weather_forecasts/synopsis.json'
+        'data/raw/daily_weather_forecasts/synopsis.json'
     )
     transformed_synopsis = transform_synopsis(
         synopsis_dataframe
     )
 
     tropical_cyclone_informations_dataframe = extract_tropical_cyclone_informations(
-        'data/daily_weather_forecasts/tropical_cyclone_informations.json'
+        'data/raw/daily_weather_forecasts/tropical_cyclone_informations.json'
     )
     transform_tropical_cyclone_informations(
         tropical_cyclone_informations_dataframe   
     )
 
     forecast_weather_conditions_dataframe = extract_forecast_weather_conditions(
-        'data/daily_weather_forecasts/forecast_weather_conditions.json'
+        'data/raw/daily_weather_forecasts/forecast_weather_conditions.json'
     )
     transformed_forecast_weather_conditions = transform_forecast_weather_conditions(
         forecast_weather_conditions_dataframe
     )
 
     forecast_wind_and_coastal_water_conditions_dataframe = extract_forecast_wind_and_coastal_water_conditions(
-        'data/daily_weather_forecasts/forecast_wind_and_coastal_water_conditions.json'
+        'data/raw/daily_weather_forecasts/forecast_wind_and_coastal_water_conditions.json'
     )
